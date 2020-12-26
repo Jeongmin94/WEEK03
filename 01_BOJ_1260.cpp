c89e5f5b14146de25ebd4929a12ca06af684f19b
Jeongmin94 <75651145+Jeongmin94@users.noreply.github.com>
2020-12-27 00:54:49 +0900

    DFS와 BFS

--- 01_BOJ_1260.cpp
+++ 01_BOJ_1260.cpp
@@ -1,45 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MX = 1001;
-int board[MX][MX];
+const int MX = 10002;
+vector<int> V[MX];
 bool vis[MX];
 int n, m, v;
-void DFS(int start) {
-    cout << start + 1 << ' ';
-    vis[start] = 1;
-    for(int i=0; i<n; i++) {
-        if(vis[i] || board[start][i] == 0) continue;
-        vis[i] = 1;
-        DFS(i);
-    }
+void dfs(int start) {
+	cout << start << ' ';
+	for (int i = 0; i < V[start].size(); i++) {
+		if (vis[V[start][i]]) continue;
+		vis[V[start][i]] = 1;
+		dfs(V[start][i]);
+	}
 }
 int main(void) {
-    cin.tie(0);
-    ios::sync_with_stdio(0);
-
-    cin >> n >> m >>v;
-
-    for(int i=0; i<m; i++) {
-        int x, y;
-        cin >> x >> y;
-        board[x-1][y-1] = board[y-1][x-1] = 1;
-    }
-    // DFS
-    DFS(v-1);
-    cout << '\n';
-    fill(vis, vis+n, 0);
-
-    // BFS
-    queue<int> Q;
-    Q.push(v-1);
-    vis[v-1] = 1;
-    while(!Q.empty()) {
-        int cur = Q.front(); Q.pop();
-        cout << cur + 1<< ' ';
-        for(int i=0; i<n; i++) {
-            if(vis[i] || board[cur][i] == 0) continue;
-            vis[i] = 1;
-            Q.push(i);
-        }
-    }
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin >> n >> m >> v;
+	for (int i = 0; i < m; i++) {
+		int x, y;
+		cin >> x >> y;
+		V[x].push_back(y);
+		V[y].push_back(x);
+	}
+	for (int i = 0; i <= n; i++) {
+		sort(V[i].begin(), V[i].end());
+	}
+	// DFS
+	vis[v] = 1;
+	dfs(v);
+	cout << '\n';
+	fill(vis, vis + MX, 0);
+	// BFS
+	queue<int> Q;
+	Q.push(v);
+	vis[v] = 1;
+	while (!Q.empty()) {
+		int cur = Q.front(); Q.pop();
+		cout << cur << ' ';
+		for (int i = 0; i < V[cur].size(); i++) {
+			if (vis[V[cur][i]]) continue;
+			Q.push(V[cur][i]);
+			vis[V[cur][i]] = 1;
+		}
+	}
 }
