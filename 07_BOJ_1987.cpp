1a9e35d8c166c1b6bdfa8ea664c8aef26c8dec4b
Jeongmin94 <75651145+Jeongmin94@users.noreply.github.com>
2020-12-26 19:29:34 +0900

    알파벳

---
+++ 07_BOJ_1987.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MX = 22;
+string board[MX];
+bool vis[26];
+int dx[4] = {-1,0,1,0};
+int dy[4] = {0,1,0,-1};
+int r, c;
+int depth;
+void dfs(int x, int y) {
+    depth++;
+    for(int dir=0; dir<4; dir++) {
+        int nx = x+dx[dir];
+        int ny = y+dy[dir];
+        if(nx<0||nx>r||ny<0||ny>=c) continue;
+        if(vis[board[nx][ny]-'A']) continue;
+        vis[board[nx][ny]-'A'] = 1;
+        dfs(nx, ny);
+        vis[board[nx][ny]-'A'] = 0;
+    }
+}
+int main(void) {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    cin >> r >> c;
+    for(int i=0; i<r; i++) {
+        cin >> board[i];
+    }
+    vis[board[0][0]-'A'] = 1;
+    dfs(0,0);
+    cout << depth;
+}
